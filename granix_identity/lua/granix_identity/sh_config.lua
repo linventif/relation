8e196ad3c80bfe92d6f44b539ef7631293c89c23
Linventif <linventif@gmail.com>
2022-11-28 00:02:18 +0100

    2022 11 28

---
+++ granix_identity/lua/granix_identity/sh_config.lua
@@ -0,0 +1,11 @@
+GranixIdentity.Lang = {}
+GranixIdentity.Config = {}
+
+GranixIdentity.Lang["unknown"] = "Inconnue"
+
+GranixIdentity.Config["AdminGroup"] = {
+    ["superadmin"] = true,
+    ["admin"] = true,
+    ["moderator"] = true,
+    ["operator"] = true
+}
